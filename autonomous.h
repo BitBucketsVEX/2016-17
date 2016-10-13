a88acef9cc619ad7fcab1efc715d00f3fe19f730
HenryTurkeyBrain <henryturkeybrain@gmail.com>
2016-10-13 15:22:42 -0700

    Add files via upload

--- autonomous.h
+++ autonomous.h
@@ -4,18 +4,19 @@
 #include "motorControlTypes.h"
 
 const float RADIUS_OF_WHEEL = 3.75 * 2.54 / 100; // in meters
-const float WHEEL_VEL = MAX_RPM * PI * RADIUS_OF_WHEEL / 30; // Max wheel RPM in m/s
+const float WHEEL_VEL = MAX_RPM * PI * RADIUS_OF_WHEEL / 30; // Max wheel speed in m/s
 
 const float MAT_LENGTH = 1.18956666667; // you guesses it! meters!
 
 
 void forward(float dist, int t) {
 	float mps = dist / t; // m / s
-	// WHEEL_RPM = k * 127
-	// k = WHEEL_RPM / 127;
-	// <some rpm> = WHEEL_RPM * instructed vel / 127
-	// instructed vel = 127 * <some rpm> / WHEEL_RPM
-	int vel = (int) 127 * mps / WHEEL_VEL;
+	// WHEEL_VEL = k * 127
+	// k = WHEEL_VEL / 127;
+	// <some rpm> = WHEEL_VEL * instructed vel / 127
+	// instructed vel = 127 * <some rpm> / WHEEL_VEL
+	int vel = (int) MAX_MOTOR_COMMAND * mps / WHEEL_VEL;
+	vel = BOUND(vel, -MAX_MOTOR_COMMAND, MAX_MOTOR_COMMAND);
 	motor[frontLeft] = motor[backLeft] = motor[frontRight] = motor[backRight] = vel;
 	turnCoef = 0;
 	wait1Msec(t * 1000);
